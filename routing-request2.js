f5b4e1bebde34a5f8bb4f6755f9f408ff80454cc
Sanjeevv30 <sanjeevsrivastava107@gmail.com>
2023-09-02 11:43:04 +0530

    redirect-product-with-form

--- routing-request2.js
+++ routing-request2.js
@@ -1,15 +1,27 @@
 const http = require("http");
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
-app.use((req, res, next) => {
-  console.log("1st middileware");
-  next();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/add-product', (req, res, next) => {
+  res.send('<form action="/product" method="POST">' +
+    '<input type="text" name="title" placeholder="Product Title"><br>' +
+    '<input type="text" name="size" placeholder="Product Size"><br>' +
+    '<button type="submit">Add Product</button>' +
+    '</form>');
 });
 
+app.use('/product', (req, res, next) => {
+  console.log(req.body); 
+  res.redirect('/');
+})
+
 app.use((req, res, next) => {
-  console.log("2nd middileware");
   res.send("<h1>Hii Sanjeev How are you!</h1>");
 });
+
 const server = http.createServer(app);
 
 server.listen(8000, () => {
