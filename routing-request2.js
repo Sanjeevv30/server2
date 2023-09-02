0538753a1dd4ac3c366acaf5392078f532975248
Sanjeevv30 <sanjeevsrivastava107@gmail.com>
2023-09-02 10:30:10 +0530

    midddleware

--- routing-request2.js
+++ routing-request2.js
@@ -1,7 +1,16 @@
 const http = require("http");
-const routes = require('./routes');
-console.log(routes.someText);
-const server = http.createServer(routes.handler);
+const express = require("express");
+const app = express();
+app.use((req, res, next) => {
+  console.log("1st middileware");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("2nd middileware");
+  res.send("<h1>Hii Sanjeev How are you!</h1>");
+});
+const server = http.createServer(app);
 
 server.listen(8000, () => {
   console.log("Server is running on port 8000");
